b64a8dee3cc2acf2cf6b731d4edca628721f8144
grey939597 <jongyunj@icloud.com>
2022-01-06 23:43:39 +0900

    #11 FEED: Hashtags

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,16 +1,38 @@
+// import sanitizeHtml from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Comment = ({ author, payload }: any) => {
+  // const cleaedPayload = sanitizeHtml(
+  //   payload.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
+  //   {
+  //     allowedTags: ["mark"],
+  //   }
+  // );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: payload.replace(
+            /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g,
+            "<mark>$&</mark>"
+          ),
+        }}
+      ></CommentCaption>
     </CommentContainer>
   );
 };
