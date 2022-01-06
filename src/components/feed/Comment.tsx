86a01eaa4c07db622c2e4fb184d6d18f10270eaa
grey939597 <jongyunj@icloud.com>
2022-01-07 00:21:28 +0900

    #11 FEED: Hashtags

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,11 +1,12 @@
-import sanitizeHtml from "sanitize-html";
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.accent};
     cursor: pointer;
@@ -16,20 +17,20 @@ const CommentCaption = styled.span`
 `;
 
 const Comment = ({ author, payload }: any) => {
-  const cleaedPayload = sanitizeHtml(
-    payload.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
-    {
-      allowedTags: ["mark"],
-    }
-  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleaedPayload,
-        }}
-      ></CommentCaption>
+      <CommentCaption>
+        {payload.split(" ").map((word: string, index: number) =>
+          /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 };
